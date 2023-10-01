1e098a7ef38c1da579abdef67b5138b6777047aa
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-02 00:36:51 +0600

    Implement delete from database  method  successfully.

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,11 +3,30 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
-    const users= useLoaderData();
+    const users = useLoaderData();
+
+    const handleDelete = (_id) => {
+        console.log('delete id:',_id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount>0){
+                    alert('successfully deleted.')
+                }
+            });
+
+
+    }
     return (
         <div>
             {
-                users.map(user=><p key={user._id}>{user.name}</p>)
+                users.map(user => <p key={user._id}>
+                    {user.name} {user.email}
+                    <button onClick={() => handleDelete(user._id)}>X</button>
+                </p>)
             }
         </div>
     );
